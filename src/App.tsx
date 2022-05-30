16085eb1f5426df605cba4d70be169eb076cb22e
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-30 17:21:15 +0300

    comm

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Login} from "./loggined/Login";
+import {useSelector} from "react-redux";
+import {AppStoreType} from "./redux/store";
+import {Profile} from "./profile/Profile";
+import {Routers} from "./router/Routes";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const isLoggedIn = useSelector<AppStoreType, boolean>(state => state.login.isLoggedIn)
+
+    return (
+        <div className="app">
+            {isLoggedIn ? <Profile/> : <Login/>}
+            <Routers/>
+        </div>
+    );
 }
 
 export default App;
