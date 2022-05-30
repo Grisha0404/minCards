4fbd89783d83136527de4abedecb607556a3054d
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-30 17:23:02 +0300

    comm

---
+++ src/cards/cardsEmployee.ts
@@ -0,0 +1,13 @@
+import Kom from '../img/0732.jpg'
+
+export const cardsEmployee = [
+    {
+        firstName: 'Ком',
+        secondName:'Григорий',
+        password: '123',
+        logo: Kom,
+        smena: 3,
+        jobTitle: 'Начальник смены'
+    }
+]
+
