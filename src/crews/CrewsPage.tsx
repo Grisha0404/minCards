d10aad27ed261676a69ed294f3a80363046f67b4
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-30 17:23:21 +0300

    comm

---
+++ src/crews/CrewsPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Crews.module.css'
+import '../App.css'
+
+export const CrewsPage = () => {
+    return (
+        <div className='content'>
+        <div className={style.appliances}>
+           <h1>Экипажи</h1>
+        </div>
+        </div>
+    );
+};
