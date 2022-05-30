3e6ab11f28dbfc131def062cbd79b37a14d15215
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-30 17:23:47 +0300

    comm

---
+++ src/schedule/SchedulePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './SchedulePage.module.css'
+import '../App.css'
+
+export const SchedulePage = () => {
+    return (
+        <div className='content'>
+        <div className={style.appliances}>
+           <h1>График работы</h1>
+        </div>
+        </div>
+    );
+};
