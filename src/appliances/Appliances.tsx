8ecb8e86f90a8ad60e8134e27d0da9e483321d91
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-30 17:16:28 +0300

    comm

---
+++ src/appliances/Appliances.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Appliances.module.css'
+import '../App.css'
+
+export const Appliances = () => {
+    return (
+        <div className='content'>
+        <div className={style.appliances}>
+           <h1> ПРИБОРЫ</h1>
+        </div>
+        </div>
+    );
+};
