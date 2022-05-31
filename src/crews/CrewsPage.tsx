faacdd051f0cb3dd8d175a49f8be64454fa7715a
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-31 20:06:12 +0300

    table

--- src/crews/CrewsPage.tsx
+++ src/crews/CrewsPage.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import style from './Crews.module.css'
 import '../App.css'
+import {Gaz} from "./gaz/Gaz";
 
 export const CrewsPage = () => {
+
     return (
         <div className='content'>
-        <div className={style.appliances}>
-           <h1>Экипажи</h1>
-        </div>
+            <div className={style.appliances}>
+                <h1>Экипажи</h1>
+                <Gaz name={'GAZ-1'}/>
+                <Gaz name={'GAZ-2'}/>
+                <Gaz name={'GAZ-3'}/>
+                <Gaz name={'GAZ-4'}/>
+            </div>
         </div>
     );
 };
